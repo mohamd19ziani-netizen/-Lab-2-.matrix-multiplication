afd2062e9880f440b019ab885799da7dbd2d7db4
mohamd19ziani-netizen <mohamd19ziani@gmail.com>
2026-06-02 02:05:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,60 +2,50 @@
 #include <complex>
 #include <vector>
 #include <chrono>
+#include <cblas.h>
 
 using namespace std;
 
 int main() {
-
     const int N = 256;
 
-    vector<vector<complex<double>>> A(
-        N,
-        vector<complex<double>>(N)
-    );
+    vector<complex<double>> A(N * N);
+    vector<complex<double>> B(N * N);
+    vector<complex<double>> C(N * N);
 
-    vector<vector<complex<double>>> B(
-        N,
-        vector<complex<double>>(N)
-    );
-
-    vector<vector<complex<double>>> C(
-        N,
-        vector<complex<double>>(N)
-    );
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            A[i][j] = complex<double>(1.0, 1.0);
-            B[i][j] = complex<double>(1.0, 1.0);
-        }
+    for (int i = 0; i < N * N; i++) {
+        A[i] = complex<double>(1.0, 1.0);
+        B[i] = complex<double>(1.0, 1.0);
     }
 
-    auto start =
-        chrono::high_resolution_clock::now();
+    complex<double> alpha(1.0, 0.0);
+    complex<double> beta(0.0, 0.0);
 
-    for (int i = 0; i < N; i++) {
+    auto start = chrono::high_resolution_clock::now();
 
-        for (int j = 0; j < N; j++) {
+    cblas_zgemm(
+        CblasRowMajor,
+        CblasNoTrans,
+        CblasNoTrans,
+        N,
+        N,
+        N,
+        &alpha,
+        A.data(),
+        N,
+        B.data(),
+        N,
+        &beta,
+        C.data(),
+        N
+    );
 
-            for (int k = 0; k < N; k++) {
-
-                C[i][j] +=
-                    A[i][k] * B[k][j];
-            }
-        }
-    }
-
-    auto end =
-        chrono::high_resolution_clock::now();
+    auto end = chrono::high_resolution_clock::now();
 
     double time =
-        chrono::duration<double>(
-            end - start
-        ).count();
+        chrono::duration<double>(end - start).count();
 
-    double operations =
-        2.0 * N * N * N;
+    double operations = 2.0 * N * N * N;
 
     double mflops =
         operations / (time * 1e6);
