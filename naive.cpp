8143cf3c86a6a6c27000b925a6e08188fc4f90a1
mohamd19ziani-netizen <mohamd19ziani@gmail.com>
2026-06-02 03:35:10 +0300

    naive.cpp code

--- naive.cpp
+++ naive.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <complex>
 #include <chrono>
+
 using namespace std;
 
-const int N = 256;
+const int N = 4096;
 
-complex<double> A[256][256], B[256][256], C[256][256];
+complex<double> A[N][N], B[N][N], C[N][N];
 
 int main() {
 
     for(int i=0;i<N;i++)
         for(int j=0;j<N;j++){
-            A[i][j] = {1,1};
-            B[i][j] = {1,1};
-            C[i][j] = {0,0};
+            A[i][j] = {1.0, 1.0};
+            B[i][j] = {1.0, 1.0};
+            C[i][j] = {0.0, 0.0};
         }
 
     auto start = chrono::high_resolution_clock::now();
@@ -27,9 +28,13 @@ int main() {
 
     double t = chrono::duration<double>(end-start).count();
 
-    double c = 2.0*N*N*N;
-    double mflops = c/(t*1e6);
+    double c = 2.0 * N * N * N;
+    double mflops = c / (t * 1e6);
 
-    cout<<"Naive time: "<<t<<endl;
-    cout<<"MFLOPS: "<<mflops<<endl;
+    cout << "Author: зиани мохаммед\n";
+    cout << "Group: 090304-РПИа-о25\n";
+    cout << "Naive Time: " << t << " sec\n";
+    cout << "MFLOPS: " << mflops << endl;
+
+    return 0;
 }
